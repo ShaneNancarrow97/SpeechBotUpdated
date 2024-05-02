cd5f3442320e550ef4fe5082aa1f87098e023cab
ShaneNancarrow97 <168131204+ShaneNancarrow97@users.noreply.github.com>
2024-05-02 09:51:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run('meta/meta-llama-3-8b-instruct', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1, "max_tokens":12288})
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1, "max_new_tokens":12288})
     return output
 
 # User-provided prompt
