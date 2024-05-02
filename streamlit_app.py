12ab232ac2fceac517623bdd7814e2cf1e34f15a
ShaneNancarrow97 <168131204+ShaneNancarrow97@users.noreply.github.com>
2024-05-02 12:13:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.sidebar:
         llm = 'meta/meta-llama-3-8b-instruct'
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=1.0, value=0.5, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.5, step=0.01)
-    max_new_tokens = st.sidebar.slider('max_new_tokens', min_value=1000, max_value=20000, value=20000, step=14)
+    max_new_tokens = st.sidebar.slider('max_new_tokens', min_value=1000, max_value=20000, value=20000, step=20)
     st.markdown('# Link to [CallMiner Analyze](https://vanquisbank.callminer.net)')
 
 # Store LLM generated responses
